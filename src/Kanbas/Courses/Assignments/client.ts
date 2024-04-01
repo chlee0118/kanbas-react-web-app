ef32f3cdfcd8c7b7a33026dc49b1bbad98ce3a2b
Daniel Lee <lee.daniel6@northeastern.edu>
2024-04-01 16:59:55 -0400

    changed API for Netlify deploy

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
-const COURSES_API = "http://localhost:4000/api/courses";
-const ASSIGNMENTS_API = "http://localhost:4000/api/assignments"
+const COURSES_API = "https://kanbas-node-server-app-apg1.onrender.com/api/courses";
+const ASSIGNMENTS_API = "https://kanbas-node-server-app-apg1.onrender.com/api/assignments"
 
-export const createAssignment = async (courseId, assignment) => {
+export const createAssignment = async (courseId: any, assignment: any) => {
   const response = await axios.post(`${COURSES_API}/${courseId}/assignments`, assignment);
   console.log(response.data);
   return response.data;
 };
 
-export const findAssignmentsForCourse = async (courseId) => {
+export const findAssignmentsForCourse = async (courseId: any) => {
   const response = await axios.get(`${COURSES_API}/${courseId}/assignments`);
   return response.data;
 };
 
-export const updateAssignment = async (assignmentId, assignment) => {
+export const updateAssignment = async (assignmentId: any, assignment: any) => {
     const response = await axios.put(`${ASSIGNMENTS_API}/${assignmentId}`, assignment);
     return response.data;
   };
 
-export const deleteAssignment = async (assignmentId) => {
+export const deleteAssignment = async (assignmentId: any) => {
   const response = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
   return response.data;
 };
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -13,7 +13,7 @@ import axios from "axios";
 
 function Courses() {
   const { courseId } = useParams();
-  const COURSES_API = "http://localhost:4000/api/courses";
+  const COURSES_API = "https://kanbas-node-server-app-apg1.onrender.com/api/courses";
   const [course, setCourse] = useState<any>({ _id: "" });
   const findCourseById = async (courseId?: string) => {
     const response = await axios.get(
